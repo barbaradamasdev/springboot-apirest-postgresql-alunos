ce4e3ccb3119ab4e3af282c1d8759c8490dfc726
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-11-02 18:16:38 -0300

    project add

---
+++ src/main/java/com/bddev/springboot/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.bddev.springboot.repositories;
+
+
+import com.bddev.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
