2282ce47a4b54030125b3a7d7ba8d613fb31163d
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-11-03 19:04:02 -0300

    swagger connection finished @operation add

--- src/main/java/com/bddev/springboot/controllers/AlunoController.java
+++ src/main/java/com/bddev/springboot/controllers/AlunoController.java
@@ -3,6 +3,7 @@ package com.bddev.springboot.controllers;
 import com.bddev.springboot.dtos.AlunoRecordDto;
 import com.bddev.springboot.models.AlunoModel;
 import com.bddev.springboot.repositories.AlunoRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,7 @@ public class AlunoController {
     @Autowired
     AlunoRepository alunoRepository;
 
+    @Operation(summary = "Adiciona um novo aluno", method = "POST")
     @PostMapping("/alunos")
     public ResponseEntity<AlunoModel> saveAluno(@RequestBody @Valid AlunoRecordDto alunoRecordDto) {
         var alunoModel = new AlunoModel();
@@ -28,20 +30,23 @@ public class AlunoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(alunoRepository.save(alunoModel));
     }
 
+    @Operation(summary = "Retorna todos os alunos", method = "GET")
     @GetMapping("/alunos")
     public ResponseEntity<List<AlunoModel>> getAllAluno(){
         return ResponseEntity.status(HttpStatus.OK).body(alunoRepository.findAll());
     }
 
+    @Operation(summary = "Busca aluno por id", method = "GET")
     @GetMapping("/alunos/{id}")
     public ResponseEntity<Object> getOneAlunos(@PathVariable(value = "id")UUID id) {
         Optional<AlunoModel> aluno0 = alunoRepository.findById(id);
         if(aluno0.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Aluno nao encontrado.");
         }
-        return ResponseEntity.status(HttpStatus.OK).body(aluno0.get());
+        return ResponseEntity.status(HttpStatus.OK).body(aluno0);
     }
 
+    @Operation(summary = "Atualiza aluno por id", method = "PUT")
     @PutMapping("/alunos/{id}")
     public ResponseEntity<Object> updateAluno(@PathVariable(value = "id")UUID id,
                                               @RequestBody @Valid AlunoRecordDto alunoRecordDto) {
@@ -50,10 +55,11 @@ public class AlunoController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Aluno nao encontrado.");
         }
         var alunoModel = aluno0.get();
-        BeanUtils.copyProperties(alunoRecordDto, aluno0);
+        BeanUtils.copyProperties(alunoRecordDto, alunoModel);
         return ResponseEntity.status(HttpStatus.OK).body(alunoRepository.save(alunoModel));
     }
 
+    @Operation(summary = "Apaga um aluno por id", method = "DELETE")
     @DeleteMapping("/alunos/{id}")
     public ResponseEntity<Object> deleteAluno(@PathVariable(value = "id")UUID id) {
         Optional<AlunoModel> aluno0 = alunoRepository.findById(id);
@@ -61,7 +67,7 @@ public class AlunoController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Aluno nao encontrado.");
         }
         alunoRepository.delete(aluno0.get());
-        return ResponseEntity.status(HttpStatus.OK).body("Aluno deletado com sucesso");
+        return ResponseEntity.status(HttpStatus.OK).body("Aluno deletado com sucesso.");
     }
 }
 
